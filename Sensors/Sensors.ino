d76b27e84ae0aa25ace7c2c8d573387de510588c
Santiago Matta <sandormtt@gmail.com>
2023-11-29 20:16:32 -0500

    .

--- Sensors/Sensors.ino
+++ Sensors/Sensors.ino
@@ -1,24 +1,56 @@
 #include <DHT.h>
 #include <Wire.h>
 
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
 #define DHTPIN 2         // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11    // DHT 11
 #define SLAVE_ADDRESS 0x08 // I2C Address for the Arduino
+
+#define B 3950 // B factor
+#define RESISTOR 100000 // resistencia del resistor, 200 kOhm
+#define THERMISTOR 100000 // resistencia nominal del termistor, 100 kOhm
+#define NOMINAL 25 // temperatura nominal
+#define sensor A1
+
+TinyGPS gps;
+SoftwareSerial ss(4, 3);
 
 DHT dht(DHTPIN, DHTTYPE);
 
 float lastHumidity = 0.0;
 float lastTemperature = 0.0;
+float termistor = 0.0;
+float latitud = 0.0;
+float longitud = 0.0;
+
+static void smartdelay(unsigned long ms);
+static void print_float(float val, float invalid, int len, int prec);
+static void print_int(unsigned long val, unsigned long invalid, int len);
+static void print_date(TinyGPS &gps);
+static void print_str(const char *str, int len);
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
   Wire.begin(SLAVE_ADDRESS);
   Wire.onRequest(sendData);
+
+  Serial.print("Testing TinyGPS library v. "); Serial.println(TinyGPS::library_version());
+  Serial.println("by Mikal Hart");
+  Serial.println();
+  Serial.println("Sats HDOP Latitude  Longitude  Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum");
+  Serial.println("          (deg)     (deg)      Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail");
+  Serial.println("-------------------------------------------------------------------------------------------------------------------------------------");
+
+  ss.begin(9600);
+
+  pinMode(sensor, INPUT);
 }
 
 void loop() {
-  delay(2000);
+  //delay(2000);
   
   float h = dht.readHumidity();
   float t = dht.readTemperature();
@@ -32,6 +64,56 @@ void loop() {
   } else {
     Serial.println("Failed to read from DHT sensor!");
   }
+
+  float flat, flon;
+  unsigned long age, date, time, chars = 0;
+  unsigned short sentences = 0, failed = 0;
+  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+  
+  print_int(gps.satellites(), TinyGPS::GPS_INVALID_SATELLITES, 5);
+  print_int(gps.hdop(), TinyGPS::GPS_INVALID_HDOP, 5);
+  gps.f_get_position(&flat, &flon, &age);
+  latitud = flat;
+  longitud = flon;
+  print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
+  print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
+  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
+  print_date(gps);
+  print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
+  print_float(gps.f_course(), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
+  print_float(gps.f_speed_kmph(), TinyGPS::GPS_INVALID_F_SPEED, 6, 2);
+  print_str(gps.f_course() == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(gps.f_course()), 6);
+  print_int(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0xFFFFFFFF : (unsigned long)TinyGPS::distance_between(flat, flon, LONDON_LAT, LONDON_LON) / 1000, 0xFFFFFFFF, 9);
+  print_float(flat == TinyGPS::GPS_INVALID_F_ANGLE ? TinyGPS::GPS_INVALID_F_ANGLE : TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
+  print_str(flat == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON)), 6);
+
+  gps.stats(&chars, &sentences, &failed);
+  print_int(chars, 0xFFFFFFFF, 6);
+  print_int(sentences, 0xFFFFFFFF, 10);
+  print_int(failed, 0xFFFFFFFF, 9);
+  Serial.println();
+
+  int te = analogRead(sensor);
+  float tr = 1023.0 / te - 1;
+  tr = RESISTOR / tr;
+  Serial.print("R=");
+  Serial.print(tr);
+  Serial.print(", t=");
+ 
+  float steinhart;
+  steinhart = tr / THERMISTOR;
+  steinhart = log(steinhart);
+  steinhart /= B;
+  steinhart += 1.0 / (NOMINAL + 273.15);
+  steinhart = 1.0 / steinhart;
+  steinhart -= 273.15; 
+  Serial.println(steinhart);
+  steinhart = termistor;
+  
+  smartdelay(1000);
+
+
+  
 }
 
 void sendData() {
@@ -40,8 +122,18 @@ void sendData() {
   floatToBytes(lastTemperature, buffer);
   Wire.write(buffer, 4);
 
-  floatToBytes(lastHumidity, buffer);
+  //floatToBytes(lastHumidity, buffer);
+  //Wire.write(buffer, 4);
+
+  floatToBytes(termistor, buffer);
   Wire.write(buffer, 4);
+
+  floatToBytes(latitud, buffer);
+  Wire.write(buffer, 4);
+
+  floatToBytes(longitud, buffer);
+  Wire.write(buffer, 4);
+  
 }
 
 void floatToBytes(float val, byte* bytes_array) {
@@ -51,4 +143,77 @@ void floatToBytes(float val, byte* bytes_array) {
   } u;
   u.float_variable = val;
   memcpy(bytes_array, u.temp_array, 4);
+}
+
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
+
+static void print_float(float val, float invalid, int len, int prec)
+{
+  if (val == invalid)
+  {
+    while (len-- > 1)
+      Serial.print('*');
+    Serial.print(' ');
+  }
+  else
+  {
+    Serial.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      Serial.print(' ');
+  }
+  smartdelay(0);
+}
+
+static void print_int(unsigned long val, unsigned long invalid, int len)
+{
+  char sz[32];
+  if (val == invalid)
+    strcpy(sz, "*******");
+  else
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i=strlen(sz); i<len; ++i)
+    sz[i] = ' ';
+  if (len > 0) 
+    sz[len-1] = ' ';
+  Serial.print(sz);
+  smartdelay(0);
+}
+
+static void print_date(TinyGPS &gps)
+{
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned long age;
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+  if (age == TinyGPS::GPS_INVALID_AGE)
+    Serial.print("********** ******** ");
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
+        month, day, year, hour, minute, second);
+    Serial.print(sz);
+  }
+  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
+  smartdelay(0);
+}
+
+static void print_str(const char *str, int len)
+{
+  int slen = strlen(str);
+  for (int i=0; i<len; ++i)
+    Serial.print(i<slen ? str[i] : ' ');
+  smartdelay(0);
 }
