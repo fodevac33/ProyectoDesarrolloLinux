ab6069727cff7a97e97563799a79c6a09fe80912
fodevac <fodevac@gmail.com>
2023-12-03 11:40:46 -0500

    .

--- i2c.cpp
+++ i2c.cpp
@@ -73,7 +73,7 @@ int main(int argc, char **argv) {
   try
   {
     std::cout << "Json: " << stream.str() << "\n";
-    http::Request request{"localhost:5173/api"};
+    http::Request request{"http://localhost:5173/api"};
     const std::string body = stream.str();
     const auto response = request.send("POST", body, {
       {"Content-Type", "application/json"}
