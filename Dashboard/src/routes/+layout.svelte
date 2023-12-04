d8638e2bd59d444a624eae41ca8b2251d3e9764f
fodevac <fodevac@gmail.com>
2023-12-03 20:26:08 -0500

    .

--- Dashboard/src/routes/+layout.svelte
+++ Dashboard/src/routes/+layout.svelte
@@ -3,10 +3,41 @@
   import { AppShell, AppBar } from '@skeletonlabs/skeleton';
   import SideBar from "$lib/SideBar.svelte";
   import type { LayoutData } from "./$types";
+  import { browser } from "$app/environment";
+  import { onMount } from "svelte";
+  import type { SvelteComponent } from "svelte";
 
   export let data: LayoutData;
 
-  let {sensorData} = data; 
+  let sideBarComp: SvelteComponent;
+
+  let { sensorData } = data; 
+  $: console.log(sensorData);
+
+  const updateSensorData = async () => {
+    try {
+      const response = await fetch('/api/latest'); // replace with your actual API endpoint
+      if (response.ok) {
+        const newData = await response.json();
+        sensorData = {...newData}; // Update sensorData with the latest data
+		sideBarComp.$set(sensorData); //
+      } else {
+        throw new Error('Failed to fetch sensor data');
+      }
+    } catch (error) {
+      console.error('Error fetching sensor data:', error);
+    }
+  }
+
+  if (browser) {
+    onMount(() => {
+      const interval = setInterval(updateSensorData, 2000); // Set up polling
+
+      return () => {
+        clearInterval(interval); // Cleanup on component destruction
+      };
+    });
+  }
 </script>
 
 
@@ -15,7 +46,7 @@
 		<AppBar class="h1 font-bold shadow-lg">Dashboard Sistema Asistido</AppBar>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
-	<SideBar {...sensorData}/>
+	<SideBar {...sensorData} bind:this={sideBarComp}/>
 	</svelte:fragment>
 	<slot />
 </AppShell>
--- Dashboard/src/routes/api/latest/+server.ts
+++ Dashboard/src/routes/api/latest/+server.ts
@@ -1,8 +1,8 @@
+import { fetchLatestReading } from '$lib/primsa';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
+    const data = await fetchLatestReading();
     
-
-    
-    return new Response();
+    return new Response(JSON.stringify(data));
 };
